ee5218aacbd5fd178cba2d7a8c9f829a730a375a
Pranavkumar Mallela <pranavmallela2020@gmail.com>
2025-07-23 18:45:26 -0400

    changed tab header

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
+    default: 'Pranav Mallela',
     template: '%s | Next.js Portfolio Starter',
   },
   description: 'This is my portfolio.',
